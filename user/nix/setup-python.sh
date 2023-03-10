419c029e3336a0face3c5f1d4e213e9480856354
Gabriele Catania <gabriele.ctn@gmail.com>
2023-03-10 17:19:10 +0000

    add python setup

---
+++ user/nix/setup-python.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# load the global configuration
+SETUP_ROOT=$(readlink -f "$(dirname "$0")/../..")
+. "$SETUP_ROOT/config.txt" || exit 1
+
+# set up python related commands
+# needs to run after apt configuration
+pipx ensurepath 1> /dev/null 2>&1
+
+pipx_install() {
+  pkg="$1"
+  if [ ! -x "$(which "$pkg")" ]; then
+    pipx install "$pkg"
+  fi
+}
+
+pipx_install pre-commit
+pipx_install cookiecutter
+
+# set up pre-commit to be automatically active for all repositories that have a configuration
+if [ ! -f "$GIT_TEMPLATE_DIR/hooks/pre-commit" ]; then
+  ~/.local/bin/pre-commit init-templatedir "$GIT_TEMPLATE_DIR"
+fi
+
+exit 0
